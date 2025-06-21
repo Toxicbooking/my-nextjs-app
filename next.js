b73b2eee9f0e23353f572b8115228c9a58125d59
Toxicbooking <toxicbooking176@gmail.com>
2025-06-20 21:24:11 -0400

    Create next.js

---
+++ next.js
@@ -0,0 +1,17 @@
+{
+  "name": "my-nextjs-app",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "13.4.4",
+    "react": "18.2.0",
+    "react-dom": "18.2.0"
+  }
+}
+export default function Home() {
+  return <h1>Hello, Next.js on GitHub + Vercel!</h1>;
+}
